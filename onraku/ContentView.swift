08b63b48b4e85770c85c65091f8f2e5740f76955
polamjag <s@polamjag.info>
2022-02-11 12:12:00 +0900

    Initial Commit

---
+++ onraku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  onraku
+//
+//  Created by Satoru Abe on 2022/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ onraku/onrakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  onrakuApp.swift
+//  onraku
+//
+//  Created by Satoru Abe on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct onrakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
