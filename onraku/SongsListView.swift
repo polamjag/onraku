4d81e0a4023b55d47cc7f26cecf873a31fdd2b54
polamjag <s@polamjag.info>
2022-02-12 14:38:12 +0900

    wip
    - menu in navigation bar
    - play / enqueue

--- onraku/SongsListView.swift
+++ onraku/SongsListView.swift
@@ -13,29 +13,66 @@ struct IdentifiableMediaItem: Identifiable {
     var item: MPMediaItem
 }
 
+let artworkSize: CGFloat = 48
+
+func playMediaItems(items: [MPMediaItem]) {
+    let collection = MPMediaItemCollection.init(items: items)
+    MPMusicPlayerController.systemMusicPlayer.setQueue(with: collection)
+    MPMusicPlayerController.systemMusicPlayer.play()
+}
+func enqueueMediaItems(items: [MPMediaItem]) {
+    let collection = MPMediaItemCollection.init(items: items)
+    let qd = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: collection)
+    MPMusicPlayerController.systemMusicPlayer.append(qd)
+    MPMusicPlayerController.systemMusicPlayer.play()
+}
+
 struct SongsListView: View {
     var songs: [MPMediaItem]
     var title: String
+    
     func getSongs() -> [IdentifiableMediaItem] {
         return self.songs.map {
             IdentifiableMediaItem(id: String($0.persistentID), item: $0)
         }
     }
+    
     var body: some View {
         List {
-            ForEach(getSongs()) { song in
-                HStack {
-                    Image(
-                        uiImage:
-                            song.item.artwork!.image(at: CGSize(width: 50, height: 50))!
-                    ).resizable().frame(width: 50, height: 50)
-                    VStack(alignment: .leading) {
-                        Text(song.item.title ?? "")
-                        Text(song.item.artist ?? "").font(.footnote).foregroundColor(.secondary)
+            Section {
+                ForEach(getSongs()) { song in
+                    HStack {
+                        Image(
+                            uiImage:
+                                song.item.artwork!.image(at: CGSize(width: artworkSize, height: artworkSize))!
+                        ).resizable().frame(width: artworkSize, height: artworkSize).cornerRadius(4)
+                        VStack(alignment: .leading) {
+                            Text(song.item.title ?? "").lineLimit(1)
+                            Text(song.item.artist ?? "").font(.footnote).foregroundColor(.secondary).lineLimit(1)
+                        }
+                    }.onTapGesture {
+                        playMediaItems(items: [song.item])
                     }
                 }
             }
-        }.navigationTitle(title)
+        }.navigationTitle(title).toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Menu {
+                    Button(action: {
+                        playMediaItems(items: songs)
+                    }) {
+                        Label("Play All", systemImage: "play")
+                    }
+                    Button(action: {
+                        enqueueMediaItems(items: songs)
+                    }) {
+                        Label("Delete", systemImage: "plus")
+                    }
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                }
+            }
+        }
     }
 }
 
