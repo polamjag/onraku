2339708992cd354da95879eab76dc1d4f94818fd
polamjag <s@polamjag.info>
2022-02-12 21:18:55 +0900

    sort

--- onraku/Views/SongsListView.swift
+++ onraku/Views/SongsListView.swift
@@ -14,14 +14,44 @@ extension MPMediaItem: Identifiable {
     }
 }
 
+enum SortSongsBy: String, Equatable, CaseIterable {
+    case none = "Default"
+    case title = "Title"
+    case album = "Album"
+    case artist = "Artist"
+    case genre = "Genre"
+    case userGrouping = "User Grouping"
+    case addedAt = "Date Added"
+}
+
 struct SongsListView: View {
     var songs: [MPMediaItem]
     var title: String
+    @State var sort: SortSongsBy = .none
+    
+    func getSortedSongs() -> [MPMediaItem] {
+        switch (sort) {
+        case .addedAt:
+            return songs.sorted { $0.dateAdded < $1.dateAdded }
+        case .title:
+            return songs.sorted { $0.title ?? "" < $1.title ?? "" }
+        case .album:
+            return songs.sorted { $0.albumTitle ?? "" < $1.albumTitle ?? "" }
+        case .artist:
+            return songs.sorted { $0.artist ?? "" < $1.artist ?? "" }
+        case .genre:
+            return songs.sorted { $0.genre ?? "" < $1.genre ?? "" }
+        case .userGrouping:
+            return songs.sorted { $0.userGrouping ?? "" < $1.userGrouping ?? "" }
+        default:
+            return songs
+        }
+    }
     
     var body: some View {
         List {
             Section(footer: Text("\(songs.count) songs")) {
-                ForEach(songs) { song in
+                ForEach(getSortedSongs()) { song in
                     NavigationLink {
                         SongDetailView(song: song)
                     } label: {
@@ -35,6 +65,11 @@ struct SongsListView: View {
             ToolbarItemGroup(placement: .navigationBarTrailing) {
                 Menu {
                     PlayableContentMenuView(target: songs)
+                    Picker("sort by", selection: $sort) {
+                        ForEach(SortSongsBy.allCases, id: \.self) { value in
+                            Text(value.rawValue).tag(value)
+                        }
+                    }
                 } label: {
                     Image(systemName: "ellipsis.circle")
                 }
