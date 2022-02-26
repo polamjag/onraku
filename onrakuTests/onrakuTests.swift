f77f568aeca22b4dc6101526ed32dbef02f5d326
polamjag <s@polamjag.info>
2022-02-26 20:53:50 +0900

    :boom:

--- onrakuTests/onrakuTests.swift
+++ onrakuTests/onrakuTests.swift
@@ -20,11 +20,6 @@ class onrakuTests: XCTestCase {
     }
 
     func testSplit() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
         XCTAssertEqual("".intelligentlySplitIntoSubArtists(), [])
         XCTAssertEqual("hoge (aa)".intelligentlySplitIntoSubArtists(), ["hoge", "aa"])
         XCTAssertEqual("a (b), c (d)".intelligentlySplitIntoSubArtists(), ["a", "b", "c", "d"])
