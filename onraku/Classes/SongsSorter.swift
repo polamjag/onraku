7cac2820f1050e1b4fe12ca14175dfb43a798576
polamjag <s@polamjag.info>
2022-11-20 21:55:49 +0900

    format

--- onraku/Classes/SongsSorter.swift
+++ onraku/Classes/SongsSorter.swift
@@ -24,32 +24,32 @@ enum SongsSortKey: String, Equatable, CaseIterable {
 func sortSongs(songs: [MPMediaItem], by key: SongsSortKey) async -> [MPMediaItem] {
     let task = Task.detached(priority: .high) { () -> [MPMediaItem] in
         switch key {
-            case .addedAt:
-                return songs.sorted { $0.dateAdded < $1.dateAdded }
-            case .title:
-                return songs.sorted { $0.title ?? "" < $1.title ?? "" }
-            case .album:
-                return songs.sorted { $0.albumTitle ?? "" < $1.albumTitle ?? "" }
-            case .artist:
-                return songs.sorted { $0.artist ?? "" < $1.artist ?? "" }
-            case .genre:
-                return songs.sorted { $0.genre ?? "" < $1.genre ?? "" }
-            case .userGrouping:
-                return songs.sorted { $0.userGrouping ?? "" < $1.userGrouping ?? "" }
-            case .bpm:
-                return songs.sorted {
-                    $0.beatsPerMinuteForSorting
+        case .addedAt:
+            return songs.sorted { $0.dateAdded < $1.dateAdded }
+        case .title:
+            return songs.sorted { $0.title ?? "" < $1.title ?? "" }
+        case .album:
+            return songs.sorted { $0.albumTitle ?? "" < $1.albumTitle ?? "" }
+        case .artist:
+            return songs.sorted { $0.artist ?? "" < $1.artist ?? "" }
+        case .genre:
+            return songs.sorted { $0.genre ?? "" < $1.genre ?? "" }
+        case .userGrouping:
+            return songs.sorted { $0.userGrouping ?? "" < $1.userGrouping ?? "" }
+        case .bpm:
+            return songs.sorted {
+                $0.beatsPerMinuteForSorting
                     < $1.beatsPerMinuteForSorting
-                }
-            case .playCountAsc:
-                return songs.sorted { $0.playCount < $1.playCount }
-            case .playCountDesc:
-                return songs.sorted { $0.playCount > $1.playCount }
-            default:
-                return songs
+            }
+        case .playCountAsc:
+            return songs.sorted { $0.playCount < $1.playCount }
+        case .playCountDesc:
+            return songs.sorted { $0.playCount > $1.playCount }
+        default:
+            return songs
         }
     }
-    
+
     do {
         return try await task.result.get()
     } catch {
