2b5c322eb07caca71da18596a0bbefd04b346bc7
polamjag <s@polamjag.info>
2025-01-04 19:32:50 +0900

    improve parentPersistentID

--- onraku/Classes/Extensions/MPMediaPlaylist+Extension.swift
+++ onraku/Classes/Extensions/MPMediaPlaylist+Extension.swift
@@ -13,8 +13,22 @@ extension MPMediaPlaylist {
   public var isFolder: Bool {
     return self.value(forProperty: "isFolder") as? Bool ?? false
   }
+
+  public var parentPersistentID: MPMediaEntityPersistentID? {
+    if let raw = self.value(forProperty: "parentPersistentID") {
+      if let val = raw as? UInt64 {
+        return val
+      } else if let val = raw as? Int64 {
+        return UInt64(Int64.max + val + 1)
+      } else {
+        return nil
+      }
+    } else {
+      return nil
+    }
+  }
   
-  public var parentPersistentID: String? {
-    return self.value(forProperty: "parentPersistentID") as? String
+  public var hasParent: Bool {
+    self.parentPersistentID != nil && self.parentPersistentID != 0
   }
 }
