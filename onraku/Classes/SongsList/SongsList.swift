9604c008b81fd73c5e03f3b63430de59412f8c3a
polamjag <s@polamjag.info>
2026-01-10 16:25:50 +0900

    wip

--- onraku/Classes/SongsList/SongsList.swift
+++ onraku/Classes/SongsList/SongsList.swift
@@ -9,28 +9,43 @@ import MediaPlayer
 
 protocol SongsList {
   func songs() -> [MPMediaItem]
+  var title: String { get }
   var shouldShowSearchCriteria: Bool { get }
   var canConfigureExactMatch: Bool { get }
+}
+
+struct SongsListFixed: SongsList {
+  let fixedSongs: [MPMediaItem]
+  let title: String
+  
+  func songs() -> [MPMediaItem] {
+    self.fixedSongs
+  }
+  var shouldShowSearchCriteria: Bool { false }
+  var canConfigureExactMatch: Bool { false }
 }
 
 struct SongsListFromPlaylist: SongsList {
   let playlist: MPMediaPlaylist
   
+  var title: String {
+    self.playlist.name ?? ""
+  }
+  
   func songs() -> [MPMediaItem] {
     []
   }
   
-  var shouldShowSearchCriteria: Bool {
-    false
-  }
-  
-  var canConfigureExactMatch: Bool {
-    false
-  }
+  var shouldShowSearchCriteria: Bool { false }
+  var canConfigureExactMatch: Bool { false }
 }
 
 struct SongsListFromPredicates : SongsList {
   let predicates: [MyMPMediaPropertyPredicate]
+  
+  var title: String {
+    "Search Result"
+  }
   
   func songs() -> [MPMediaItem] {
     []
